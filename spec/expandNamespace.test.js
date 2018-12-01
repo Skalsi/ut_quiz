8a441190d37369907256ae0df0706928b1f8a628
Sumeet Kalsi <sksumeetkalsi@gmail.com>
2018-12-01 13:25:19 +0530

    add test cases for expandNamespace

--- spec/expandNamespace.test.js
+++ spec/expandNamespace.test.js
@@ -15,7 +15,45 @@ describe( 'The ./lib/expandNamespaces function', ()=>{
 
   } );
 
-  it.skip( 'start writing tests here', ()=>{
+  it( 'should return path for defined namespace', ()=>{
+
+    const expandNamespaces = proxyquire( '../lib/expandNamespace', {
+      './loadNamespaces': sinon.stub().returns( {
+        namespaces: { User: './some/test/path/User' }
+      } )
+    } );
+
+    const expected = './some/test/path/User/Contact';
+    const actual   = expandNamespaces( '<User>/Contact', './' );
+
+    expect( actual ).to.equal( expected );
+
+  } );
+
+  it( 'should return correct relative path for defined namespace', ()=>{
+
+    const expandNamespaces = proxyquire( '../lib/expandNamespace', {
+      './loadNamespaces': sinon.stub().returns( {
+        namespaces: { User: './some/test/path/User' }
+      } )
+    } );
+
+    const expected = '../../../test/path/User/Contact';
+    const actual   = expandNamespaces( '<User>/Contact', './some/other/test/path' );
+
+    expect( actual ).to.equal( expected );
+
+  } );
+
+  it( 'should throw error for undefined namespace', ()=>{
+
+    const expandNamespaces = proxyquire( '../lib/expandNamespace', {
+      './loadNamespaces': sinon.stub().returns( {
+        namespaces: { User: './some/test/path/User' }
+      } )
+    } );
+
+    expect( ()=>expandNamespaces( '<Random>/Contact', './' ) ).to.throw( 'namespace <Random> is not defined' );
 
   } );
 
